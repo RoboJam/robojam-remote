98032aaf1dfa277ede7035d02d5f18fa51b88204
outlandkarasu <outland.karasu@gmail.com>
2014-07-26 15:35:21 +0900

    refactoring

--- src/app.js
+++ src/app.js
@@ -2,63 +2,77 @@
 
 var RJ = RJ || {};
 (function(){
-        var WebSocketServer = require('websocket').server;
-        var http = require('http');
-        var fs = require('fs');
-	var SerialPort = require("serialport").SerialPort;
-        var clientHtml = fs.readFileSync('client.html');
-        var PORT = 8080;
-	var SERIAL_PORT = "/dev/ttyACM0";
-	var port = new SerialPort(SERIAL_PORT, {
-		baudRate: 115200,
-		dataBits: 8,
-		parity: "none",
-		stopBits: 1,
-		flowControl: false
-	});
-        var plainHttpServer;
-        var wsServer;
+    var WebSocketServer = require('websocket').server;
+    var http = require('http');
+    var fs = require('fs');
+    var SerialPort = require("serialport").SerialPort;
+    var clientHtml = fs.readFileSync('client.html');
+    var PORT = 8080;
+    var SERIAL_PORT = "/dev/ttyACM0";
+    var port = new SerialPort(SERIAL_PORT, {
+        baudRate: 115200,
+	dataBits: 8,
+	parity: "none",
+	stopBits: 1,
+	flowControl: false
+    });
+    var plainHttpServer;
+    var wsServer;
 
-        plainHttpServer = http.createServer( function(req, res) {
-                res.writeHead(200, { 'Content-Type': 'text/html'});
-                res.end(clientHtml);
+    var log;
+    var onMessage;
+    var onSerialData;
+
+    /* logging function */
+    log = function(msg) {
+        console.log(new Date() + " " + msg);
+    };
+
+    /* WebSocket meessage handler */
+    onMessage = function(message) {
+        var data = JSON.parse(message.utf8Data);
+        log(data.direction);
+        port.write(data.direction, function(error, results) {
+            log("serial error:" + error);
+            log("serial result:" + results);
         });
+    };
 
-        wsServer = new WebSocketServer({httpServer: plainHttpServer});
+    /* Serial port handler */
+    onSerialData = function(input) {
+        if(new String(input).trim().length > 0) {
+	    log("receive from serial:" + input);
+        }
+    };
 
-	port.on("open", function() {
-		console.log("open serialport!");
-        	plainHttpServer.listen(8080, function() {
-                	console.log((new Date()) + ' Server is listening on port 8080');
-        	});
-	});
-
-        wsServer.on('request', function(request) {
-                var connection;
-
-                console.log((new Date()) + ' request ' + request.origin);
-                connection = request.accept(null, request.origin);
-                console.log((new Date()) + ' Connection accepted. Peer ' + connection.remoteAddress);
-
-                connection.on('message', function(message) {
-                        var data = JSON.parse(message.utf8Data);
-                        console.log(data);
-
-			port.write(data.direction, function(error, results) {
-				console.log("serial error:" + error);
-				console.log("serial result:" + results);
-			});
-                }); 
-
-                connection.on('close', function(reasonCode, description) {
-                        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
-                });
+    /* set up serial port */
+    port.on("open", function() {
+        /* startup server after opened serial port */
+        log("open serialport!");
+        plainHttpServer.listen(PORT, function() {
+            log("Server is listening on port " + PORT);
         });
+    });
+    port.on("data", onSerialData);
 
-	port.on("data", function(input) {
-		if(new String(input).trim().length > 0) {
-			console.log("receive from serial:" + input);
-		}
-	});
+    /* HTTP server for static files. */
+    plainHttpServer = http.createServer( function(req, res) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(clientHtml);
+    });
+
+    wsServer = new WebSocketServer({httpServer: plainHttpServer});
+    wsServer.on("request", function(request) {
+        var connection;
+        log("request " + request.origin);
+        connection = request.accept(null, request.origin);
+        log("Connection accepted. Peer " + connection.remoteAddress);
+
+        connection.on("message", onMessage); 
+        connection.on("close", function(reasonCode, description) {
+            log("Peer " + connection.remoteAddress + " disconnected.");
+        });
+    });
+
 })();
 
