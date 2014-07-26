3c80cf604c69b783d3205215180d0b54855f71fc
outlandkarasu <outland.karasu@gmail.com>
2014-07-26 13:59:22 +0900

    update serial

--- src/app.js
+++ src/app.js
@@ -5,8 +5,17 @@ var RJ = RJ || {};
         var WebSocketServer = require('websocket').server;
         var http = require('http');
         var fs = require('fs');
+	var SerialPort = require("serialport").SerialPort;
         var clientHtml = fs.readFileSync('client.html');
         var PORT = 8080;
+	var SERIAL_PORT = "/dev/ttyACM0";
+	var port = new SerialPort(SERIAL_PORT, {
+		baudRate: 115200,
+		dataBits: 8,
+		parity: "none",
+		stopBits: 1,
+		flowControl: false
+	});
         var plainHttpServer;
         var wsServer;
 
@@ -14,11 +23,16 @@ var RJ = RJ || {};
                 res.writeHead(200, { 'Content-Type': 'text/html'});
                 res.end(clientHtml);
         });
-        plainHttpServer.listen(8080, function() {
-                console.log((new Date()) + ' Server is listening on port 8080');
-        });
 
         wsServer = new WebSocketServer({httpServer: plainHttpServer});
+
+	port.on("open", function() {
+		console.log("open serialport!");
+        	plainHttpServer.listen(8080, function() {
+                	console.log((new Date()) + ' Server is listening on port 8080');
+        	});
+	});
+
         wsServer.on('request', function(request) {
                 var connection;
 
@@ -27,13 +41,24 @@ var RJ = RJ || {};
                 console.log((new Date()) + ' Connection accepted. Peer ' + connection.remoteAddress);
 
                 connection.on('message', function(message) {
-                        var data = JSON.parse(msg.utf8Data);
+                        var data = JSON.parse(message.utf8Data);
                         console.log(data);
+
+			port.write(data.direction, function(error, results) {
+				console.log("serial error:" + error);
+				console.log("serial result:" + results);
+			});
                 }); 
 
                 connection.on('close', function(reasonCode, description) {
                         console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
                 });
         });
+
+	port.on("data", function(input) {
+		if(new String(input).trim().length > 0) {
+			console.log("receive from serial:" + input);
+		}
+	});
 })();
 
