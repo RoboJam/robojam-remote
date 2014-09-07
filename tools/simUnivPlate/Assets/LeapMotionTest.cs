57a20de91251f8a5fea24f304accc3b0cb7cf0f0
haiatto <haiattocc@gmail.com>
2014-09-08 01:51:44 +0900

    りーぷモーション追加

---
+++ tools/simUnivPlate/Assets/LeapMotionTest.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+public class LeapMotionTest : MonoBehaviour {
+    Controller controller;
+
+	// Use this for initialization
+	void Start () {
+        controller = new Controller();
+	}
+
+	// Update is called once per frame
+	void Update () {
+       var frame = controller.Frame();
+	
+	}
+}
